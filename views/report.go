ac4949854e1e3f5c4851155f95298681c570b4c6
Génesis Salazar <gsalazar@altus.co.cr>
2018-10-24 21:07:12 -0600

    Get report with date filter

--- views/report.go
+++ views/report.go
@@ -9,7 +9,25 @@ import (
 )
 
 func GetReport(c *gin.Context) {
-	sqlString := "(SELECT CONCAT('Cabina ', c.cabin_number::text) as description, CONCAT(ct.quantity::text, ' horas') as amount, r.total as price, r.check_in::timestamp without time zone  as date_time FROM rents r INNER JOIN contracted_times ct ON ct.id = r.fk_contracted_time INNER JOIN cabins c ON c.id = r.fk_cabin) UNION (SELECT i.name, SUM(si.amount)::TEXT, SUM(si.price), null FROM sales s INNER JOIN sale_item si ON si.fk_sale = s.id INNER JOIN items i ON i.id = si.fk_item GROUP BY i.name) ORDER BY date_time"
+
+	log.Println(c.Query("fromDate"))
+	log.Println(c.Query("toDate"))
+
+	sqlString := "(SELECT CONCAT('Cabina ', c.cabin_number::text) as description, CONCAT(ct.quantity::text, ' horas') as amount, r.total as price, r.check_in::timestamp without time zone  as date_time FROM rents r INNER JOIN contracted_times ct ON ct.id = r.fk_contracted_time INNER JOIN cabins c ON c.id = r.fk_cabin "
+
+	if c.Query("fromDate") != "" && c.Query("toDate") != "" {
+		sqlString = sqlString + "WHERE r.check_in > '" + c.Query("fromDate") + "' and r.check_in < '" + c.Query("toDate") + "') "
+	}
+
+	sqlString = sqlString + "UNION (SELECT i.name, SUM(si.amount)::TEXT, SUM(si.price), null FROM sales s INNER JOIN sale_item si ON si.fk_sale = s.id INNER JOIN items i ON i.id = si.fk_item"
+
+	if c.Query("fromDate") != "" && c.Query("toDate") != "" {
+		sqlString = sqlString + " WHERE s.date > '" + c.Query("fromDate") + "' and s.date < '" + c.Query("toDate") + "'"
+	}
+
+	sqlString = sqlString + " GROUP BY i.name) ORDER BY date_time"
+
+	log.Println(sqlString)
 
 	rows, err := database.DB.Query(sqlString)
 	if err != nil {
